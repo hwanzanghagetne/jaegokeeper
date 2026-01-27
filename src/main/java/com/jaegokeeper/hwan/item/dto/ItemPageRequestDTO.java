b9b2e4a5e9f416b1c4e532b764ce0d82c656fa4c
hwanzanghagetne <dltmdghks543@gmail.com>
2026-01-27 19:36:18 +0900

    fix: 스톡매퍼 수정

--- src/main/java/com/jaegokeeper/hwan/item/dto/ItemPageRequestDTO.java
+++ src/main/java/com/jaegokeeper/hwan/item/dto/ItemPageRequestDTO.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Getter
 @Setter
